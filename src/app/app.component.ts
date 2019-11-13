222f0300638d34f3a084485df96e07603558332f
hemme82 <davidhemmesch@gmail.com>
2019-11-13 15:44:21 -0600

    Understanding dependency injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TestService } from './test.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'third-project';
+
+  // I need TestService....dependency injection
+  constructor(private svc: TestService){
+    this.svc.printToConsole("Got the Service");
+  }
+
+  //private svc: is the the same as
+  //private svc: TestService   (member variable)....then in constructor 
+  //this.svc = svc;
+  
 }
